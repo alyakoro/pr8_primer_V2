eb2f08053f3fc3785d801350891da56a513f395d
alyak <alya.korobeynikova99@gmail.com>
2024-03-21 15:28:58 +0300

    0.2

--- app/src/main/java/com/korobeynikova/pr8_primer_v2/MainActivity.kt
+++ app/src/main/java/com/korobeynikova/pr8_primer_v2/MainActivity.kt
@@ -2,10 +2,43 @@ package com.korobeynikova.pr8_primer_v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.korobeynikova.pr8_primer_v2.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loseButton.isEnabled = false
+        binding.loseButton.isEnabled = false
+
+        binding.startButton.setOnClickListener {
+            button()
+        }
+    }
+
+    private fun button() {
+        binding.loseButton.isEnabled = true
+        binding.loseButton.isEnabled = true
+        binding.startButton.isEnabled = false
+
+        generatePrimer()
+    }
+
+    private fun generatePrimer(){
+
+        val numberOne = Random.nextInt(10, 100)
+        val numberTwo = Random.nextInt(10, 100)
+        val operators = arrayOf('+', '-', '*', '/')
+
+        val operator = operators.random()
+
+        binding.nullNull1.text = numberOne.toString()
+        binding.nullNull2.text = numberTwo.toString()
+        binding.znak.text = operator.toString()
     }
 }
